235a4273a2f7f09fad4a393b2bbfa9717ff36509
cuongnd <nguyendinhcuong@gmail.com>
2017-03-22 14:30:59 +0700

    new up date

--- app_window/bho-android-master/app/src/main/java/vantinviet/banhangonline88/ux/fragments/fragment_template_sample.java
+++ app_window/bho-android-master/app/src/main/java/vantinviet/banhangonline88/ux/fragments/fragment_template_sample.java
@@ -1,87 +1,29 @@
 package vantinviet.banhangonline88.ux.fragments;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
-import android.widget.Spinner;
-import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.facebook.share.model.ShareLinkContent;
-import com.facebook.share.widget.MessageDialog;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import mbanje.kurt.fabbutton.FabButton;
 import timber.log.Timber;
-import vantinviet.banhangonline88.BuildConfig;
 import vantinviet.banhangonline88.CONST;
 import vantinviet.banhangonline88.MyApplication;
 import vantinviet.banhangonline88.R;
-import vantinviet.banhangonline88.SettingsMy;
-import vantinviet.banhangonline88.api.EndPoints;
-import vantinviet.banhangonline88.api.GsonRequest;
-import vantinviet.banhangonline88.api.JsonRequest;
 import vantinviet.banhangonline88.entities.Page;
-import vantinviet.banhangonline88.entities.User;
 import vantinviet.banhangonline88.entities.drawerMenu.DrawerMenuItem;
-import vantinviet.banhangonline88.entities.product.Product;
-import vantinviet.banhangonline88.entities.product.ProductColor;
-import vantinviet.banhangonline88.entities.product.ProductSize;
-import vantinviet.banhangonline88.entities.product.ProductVariant;
-import vantinviet.banhangonline88.interfaces.LoginDialogInterface;
-import vantinviet.banhangonline88.interfaces.ProductImagesRecyclerInterface;
-import vantinviet.banhangonline88.interfaces.RelatedProductsRecyclerInterface;
-import vantinviet.banhangonline88.interfaces.RequestListener;
-import vantinviet.banhangonline88.listeners.OnSingleClickListener;
-import vantinviet.banhangonline88.utils.Analytics;
-import vantinviet.banhangonline88.utils.JsonUtils;
-import vantinviet.banhangonline88.utils.MsgUtils;
-import vantinviet.banhangonline88.utils.RecyclerMarginDecorator;
-import vantinviet.banhangonline88.utils.Utils;
 import vantinviet.banhangonline88.ux.MainActivity;
-import vantinviet.banhangonline88.ux.adapters.ColorSpinnerAdapter;
-import vantinviet.banhangonline88.ux.adapters.ProductImagesRecyclerAdapter;
-import vantinviet.banhangonline88.ux.adapters.RelatedProductsRecyclerAdapter;
-import vantinviet.banhangonline88.ux.adapters.SizeVariantSpinnerAdapter;
-import vantinviet.banhangonline88.ux.dialogs.LoginDialogFragment;
-import vantinviet.banhangonline88.ux.dialogs.LoginExpiredDialogFragment;
-import vantinviet.banhangonline88.ux.dialogs.ProductImagesDialogFragment;
 
 /**
  * Fragment shows a detail of the product.
  */
 @SuppressLint("ValidFragment")
-public class fragment_template_vina_bonnie extends Fragment {
+public class fragment_template_sample extends Fragment {
 
     private final DrawerMenuItem drawerMenuItem;
     private final Page page;
@@ -97,7 +39,7 @@ public class fragment_template_vina_bonnie extends Fragment {
     private ViewTreeObserver.OnScrollChangedListener scrollViewListener;
     private MyApplication app;
     @SuppressLint("ValidFragment")
-    public fragment_template_vina_bonnie(DrawerMenuItem drawerMenuItem, Page page) {
+    public fragment_template_sample(DrawerMenuItem drawerMenuItem, Page page) {
         this.drawerMenuItem=drawerMenuItem;
         this.page=page;
     }
