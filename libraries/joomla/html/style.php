012efe6d01752b5a6705beff3d8ce7aa6fe44bef
cuongnd <nguyendinhcuong@gmail.com>
2017-05-01 17:31:59 +0700

    new up

--- libraries/joomla/html/style.php
+++ libraries/joomla/html/style.php
@@ -38,51 +38,44 @@ class style
                 break;
         }
     }
-    public static function padding(&$list_style,$style){
-        preg_match_all ("/(.*)px/U", $style, $pat_array);
-        $pat_array=$pat_array[1];
-        switch (count($pat_array)) {
-            case 1:
-                $list_style->padding_left=$pat_array[0]."px";
-                $list_style->padding_top=$pat_array[0]."px";
-                $list_style->padding_right=$pat_array[0]."px";
-                $list_style->padding_bottom=$pat_array[0]."px";
-                break;
-            case 2:
-                $list_style->padding_left=$pat_array[0]."px";
-                $list_style->padding_top=$pat_array[1]."px";
-                $list_style->padding_right=$pat_array[0]."px";
-                $list_style->padding_bottom=$pat_array[1]."px";
-                break;
-            case 3:
-                $list_style->padding_left=$pat_array[0]."px";
-                $list_style->padding_top=$pat_array[1]."px";
-                $list_style->padding_right=$pat_array[2]."px";
-                $list_style->padding_bottom=$pat_array[2]."px";
-                break;
-            case 4:
-                $list_style->padding_left=$pat_array[0]."px";
-                $list_style->padding_top=$pat_array[1]."px";
-                $list_style->padding_right=$pat_array[2]."px";
-                $list_style->padding_bottom=$pat_array[3]."px";
-                break;
-        }
-    }
     public static function margin(&$list_style,$style){
-        preg_match_all ("/(.*)px/U", $style, $pat_array);
-        $pat_array=$pat_array[1];
+        $pat_array = preg_split('/\s+/', trim($style));
         switch (count($pat_array)) {
             case 1:
-                $list_style->margin_left=$pat_array[0]."px";
-                $list_style->margin_top=$pat_array[0]."px";
-                $list_style->margin_right=$pat_array[0]."px";
-                $list_style->margin_bottom=$pat_array[0]."px";
+                $pat_array_0=$pat_array[0];
+                if( strpos( $pat_array_0, "px") !== false ) {
+                    $list_style->margin_top=$pat_array_0;
+                    $list_style->margin_right=$pat_array_0;
+                    $list_style->margin_bottom=$pat_array_0;
+                    $list_style->margin_left=$pat_array_0;
+                }elseif($pat_array_0=="auto"){
+                    $pat_array_0="0px";
+                    $list_style->margin_top=$pat_array_0;
+                    $list_style->margin_right=$pat_array_0;
+                    $list_style->margin_bottom=$pat_array_0;
+                    $list_style->margin_left=$pat_array_0;
+                }
                 break;
             case 2:
-                $list_style->margin_left=$pat_array[0]."px";
-                $list_style->margin_top=$pat_array[1]."px";
-                $list_style->margin_right=$pat_array[0]."px";
-                $list_style->margin_bottom=$pat_array[1]."px";
+                $pat_array_0=$pat_array[0];
+                $pat_array_1=$pat_array[1];
+                if( strpos( $pat_array_0, "px") !== false ||$pat_array_0=="0") {
+                    $pat_array_0=$pat_array_0=="0"?"0px":$pat_array_0;
+                    $list_style->margin_top=$pat_array_0;
+                    $list_style->margin_bottom=$pat_array_0;
+                }elseif($pat_array_0=="auto"){
+                    $list_style->margin_top=$pat_array_0;
+                    $list_style->margin_bottom=$pat_array_0;
+                }
+                if( strpos( $pat_array_1, "px") !== false ||$pat_array_1=="0" ) {
+                    $pat_array_1=$pat_array_1=="0"?"0px":$pat_array_1;
+                    $list_style->margin_left=$pat_array_1;
+                    $list_style->margin_right=$pat_array_1;
+                }elseif($pat_array_1=="auto"){
+                    $list_style->margin_left=$pat_array_1;
+                    $list_style->margin_right=$pat_array_1;
+
+                }
                 break;
             case 3:
                 $list_style->margin_left=$pat_array[0]."px";
@@ -95,6 +88,59 @@ class style
                 $list_style->margin_top=$pat_array[1]."px";
                 $list_style->margin_right=$pat_array[2]."px";
                 $list_style->margin_bottom=$pat_array[3]."px";
+                break;
+        }
+    }
+    public static function padding(&$list_style,$style){
+        $pat_array = preg_split('/\s+/', trim($style));
+        switch (count($pat_array)) {
+            case 1:
+                $pat_array_0=$pat_array[0];
+                if( strpos( $pat_array_0, "px") !== false ) {
+                    $list_style->padding_top=$pat_array_0;
+                    $list_style->padding_right=$pat_array_0;
+                    $list_style->padding_bottom=$pat_array_0;
+                    $list_style->padding_left=$pat_array_0;
+                }elseif($pat_array_0=="auto"){
+                    $pat_array_0="0px";
+                    $list_style->padding_top=$pat_array_0;
+                    $list_style->padding_right=$pat_array_0;
+                    $list_style->padding_bottom=$pat_array_0;
+                    $list_style->padding_left=$pat_array_0;
+                }
+                break;
+            case 2:
+                $pat_array_0=$pat_array[0];
+                $pat_array_1=$pat_array[1];
+                if( strpos( $pat_array_0, "px") !== false ||$pat_array_0=="0") {
+                    $pat_array_0=$pat_array_0=="0"?"0px":$pat_array_0;
+                    $list_style->padding_top=$pat_array_0;
+                    $list_style->padding_bottom=$pat_array_0;
+                }elseif($pat_array_0=="auto"){
+                    $list_style->padding_top=$pat_array_0;
+                    $list_style->padding_bottom=$pat_array_0;
+                }
+                if( strpos( $pat_array_1, "px") !== false ||$pat_array_1=="0" ) {
+                    $pat_array_1=$pat_array_1=="0"?"0px":$pat_array_1;
+                    $list_style->padding_left=$pat_array_1;
+                    $list_style->padding_right=$pat_array_1;
+                }elseif($pat_array_1=="auto"){
+                    $list_style->padding_left=$pat_array_1;
+                    $list_style->padding_right=$pat_array_1;
+
+                }
+                break;
+            case 3:
+                $list_style->padding_left=$pat_array[0]."px";
+                $list_style->padding_top=$pat_array[1]."px";
+                $list_style->padding_right=$pat_array[2]."px";
+                $list_style->padding_bottom=$pat_array[2]."px";
+                break;
+            case 4:
+                $list_style->padding_left=$pat_array[0]."px";
+                $list_style->padding_top=$pat_array[1]."px";
+                $list_style->padding_right=$pat_array[2]."px";
+                $list_style->padding_bottom=$pat_array[3]."px";
                 break;
         }
     }
