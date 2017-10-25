a10a4cca5f2ea1258ef9c1f887f80c6baca0ffc2
cuongnd <nguyendinhcuong@gmail.com>
2017-10-25 23:00:44 +0700

    tesst

--- GitAutoDeploy.py
+++ GitAutoDeploy.py
@@ -6,7 +6,7 @@ from subprocess import call
 
 class GitAutoDeploy(BaseHTTPRequestHandler):
 
-    CONFIG_FILEPATH = './GitAutoDeploy.conf.json'
+    CONFIG_FILEPATH = '/home/bho/domains/test.banhangonline88.com/public_html/GitAutoDeploy.conf.json'
     config = None
     quiet = False
     daemon = False
